8ab9befc9a174ea2f44b71a78d4d020533e9259e
Michael Bouwer <michaelbouwer@Michaels-MBP.lan>
2018-03-31 13:24:29 +0200

    added help modal && broke logic into seperate files

---
+++ js/user_inputs.js
@@ -0,0 +1,53 @@
+    //event listener to handle user input
+    window.addEventListener("keydown", function (e) {
+        e.preventDefault();
+        key = e.keyCode
+        //console.log(key);
+        switch (key) {
+            case 87: //W
+                y_increment -= 0.5
+                break
+            case 83: //S
+                y_increment += 0.5
+                break
+            case 65: //A
+                x_increment -= 0.5
+                break
+            case 68: //D
+                x_increment += 0.5
+                break
+            case 32: //Space
+                var ty = setInterval(() => {
+                    if (y_increment > 0) {
+                        y_increment -= 0.5
+                    } else if (y_increment < 0) {
+                        y_increment += 0.5
+                    } else if (y_increment === 0) {
+                        clearTimeout(ty)
+                    }
+                }, 100)
+
+                var tx = setInterval(() => {
+                    if (x_increment > 0) {
+                        x_increment -= 0.5
+                    } else if (x_increment < 0) {
+                        x_increment += 0.5
+                    } else if (x_increment === 0) {
+                        clearTimeout(tx)
+                    }
+                }, 100)
+                break
+            case 69: //E
+                shift();
+                break
+            case 81: //Q
+                layPrism();
+                break
+            case 76: //L
+                log("adhoc");
+                break
+            default:
+                console.log("no action");
+                break;
+        }
+    })
